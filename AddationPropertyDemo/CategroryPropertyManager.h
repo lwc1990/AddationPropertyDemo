3e800d17b8fadb566845d575e34781351208e30b
luwenchao <lwc_funnylife@126.com>
2019-10-31 16:56:13 +0800

    添加注释

--- AddationPropertyDemo/CategroryPropertyManager.h
+++ AddationPropertyDemo/CategroryPropertyManager.h
@@ -7,14 +7,14 @@
 //
 
 #import <Foundation/Foundation.h>
-
+/*分类添加属性列表的管理类*/
 NS_ASSUME_NONNULL_BEGIN
 
 @interface CategroryPropertyManager : NSObject
-@property (nonatomic,strong,readonly) NSMutableDictionary *assistPropertyMap;
+@property (nonatomic,strong,readonly) NSMutableDictionary *assistPropertyMap;//分类属性列表
 +(instancetype)manager;
--(void)addPropertyValue:(id)value propertyName:(id)key;
--(id)propertyValueForName:(NSString *)key;
+-(void)addPropertyValue:(id)value propertyName:(id)key;//添加属性方法
+-(id)propertyValueForName:(NSString *)key;//取属性方法
 @end
 
 NS_ASSUME_NONNULL_END
